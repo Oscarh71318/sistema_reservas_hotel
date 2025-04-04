33f3174a903b451c35395098dfac01f37e45dab3
Oscarh71318 <oscarh71318@gmail.com>
2025-04-04 15:37:15 -0500

    Agregada clase Cliente y prueba en main.py

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ h2 = Habitacion(numero=202, tipo="sencilla", precio=50.000,disponible=False)
 
 h1.mostrar_info()
 h2.mostrar_info()
+
+from models.cliente import Cliente
+
+# Crear un cliente
+cliente1 = Cliente("Oscar Holguin ", "713182020", "Oscar@gmail.com", "3016124433")
+
+# muesta info cliente
+cliente1.mostrar_info()
---
+++ models/cliente.py
@@ -0,0 +1,9 @@
+class Cliente:
+    def __init__(self, nombre, documento, correo, telefono):
+        self.nombre = nombre
+        self.documento = documento  # Ej: número de cédula o pasaporte
+        self.correo = correo
+        self.telefono = telefono
+
+    def mostrar_info(self):
+        print(f"Cliente: {self.nombre} | Documento: {self.documento} | Correo: {self.correo} | Teléfono: {self.telefono}")
